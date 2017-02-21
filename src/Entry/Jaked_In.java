7bce8a63ac1d8f9c0357116f8632d312360d9ed4
Flameo326 <Flameo326@10.0.0.173>
2017-02-21 12:43:06 -0700

    Setting Up initial GUI

--- src/Entry/Jaked_In.java
+++ src/Entry/Jaked_In.java
@@ -1,9 +1,19 @@
 package Entry;
 
-public class Jaked_In {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Jaked_In extends Application {
 
 	public static void main(String[] args) {
-		
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Jak'd In");
+		stage.centerOnScreen();
+		stage.show();
 	}
 
 }
