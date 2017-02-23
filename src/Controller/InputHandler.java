df0618594389a9b90be9bf5dd27ca8459577c117
Flameo326 <Flameo326@10.0.0.173>
2017-02-22 18:34:18 -0700

    Changed Input Handler To Built In Enums for simplicity

--- src/Controller/InputHandler.java
+++ src/Controller/InputHandler.java
@@ -2,25 +2,37 @@ package Controller;
 
 import java.util.HashSet;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class InputHandler {
 	
+	public final static KeyCode Player1Up = KeyCode.W;
+	public final static KeyCode Player1Down = KeyCode.S;
+	public final static KeyCode Player1Left = KeyCode.A;
+	public final static KeyCode Player1Right = KeyCode.D;
+	public final static KeyCode Player1Attack = KeyCode.SPACE;
+	public final static KeyCode Player2Up = null;
+	public final static KeyCode Player2Down = null;
+	public final static KeyCode Player2Left = null;
+	public final static KeyCode Player2Right = null;
+	public final static KeyCode Player2Attack = null;
+	
 	// We can Add more Relevant Input Things here if Needed
-	private static HashSet<String> keyInput = new HashSet<>();
+	private static HashSet<KeyCode> keyInput = new HashSet<>();
 	
 	public static void keyPress(KeyEvent e){
-		if(!keyInput.contains(e.getText())){
-			keyInput.add(e.getText());
+		if(!keyInput.contains(e.getCode())){
+			keyInput.add(e.getCode());
 		}
 	}
 	
 	public static void keyRelease(KeyEvent e){
-		keyInput.remove(e.getText());
+		keyInput.remove(e.getCode());
 	}
 	
-	public static boolean keyInputContains(String s){
-		return keyInput.contains(s);
+	public static boolean keyInputContains(KeyCode k){
+		return keyInput.contains(k);
 	}
 
 }
