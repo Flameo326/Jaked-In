d905f424179ba6d75905b84a08018ffc0c64795a
Adam Holbert Neumont <Adam Holbert Neumont@10.10.17.13>
2017-02-24 16:37:35 -0700

    Edited the player direction

--- src/Models/Players/HumanPlayer.java
+++ src/Models/Players/HumanPlayer.java
@@ -3,11 +3,13 @@ package Models.Players;
 import java.util.ArrayList;
 
 import Controller.InputHandler;
+import Enums.Direction;
 import Models.Entity;
 import Models.Weapon.HitBox;
 import Models.Weapon.MeleeWeapon;
 import SpriteSheet.SpriteSheet;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 
 public class HumanPlayer extends PlayableCharacter{
@@ -20,7 +22,7 @@ public class HumanPlayer extends PlayableCharacter{
 		setTag("Human." + ++humanID);
 		setWeapon(new MeleeWeapon(this, SpriteSheet.getBorderedBlock(20, 20, Color.WHITE)));
 		// Set Intiial Direction so it's not at 0, 0 or unmoving
-		setCurrXDir(1);
+		setCurrDir(Direction.RIGHT);
 	}
 	
 	// Right now Direction defaults to 0 if you are not moving, we should default it to a normal one if they haven't moved
@@ -28,32 +30,7 @@ public class HumanPlayer extends PlayableCharacter{
 	@Override
 	public void update(ArrayList<Entity> entities){
 		if(getTag().equals("Human.1")){
-			if(InputHandler.keyInputContains(InputHandler.Player1Up)){
-				if(!InputHandler.keyInputContains(InputHandler.Player1Down)){
-					move(0, -1);
-					setCurrYDir(-1);
-				}
-			} else if(InputHandler.keyInputContains(InputHandler.Player1Down)){
-				move(0, 1);
-				setCurrYDir(1);
-			} else {
-				if(getCurrXDir() != 0){
-					setCurrYDir(0);
-				}
-			}
-			if(InputHandler.keyInputContains(InputHandler.Player1Left)){
-				if(!InputHandler.keyInputContains(InputHandler.Player1Right)){
-					move(-1, 0);
-					setCurrXDir(-1);
-				}
-			} else if(InputHandler.keyInputContains(InputHandler.Player1Right)){
-				move(1,  0);
-				setCurrXDir(1);
-			} else{
-				if(getCurrYDir() != 0){
-					setCurrXDir(0);
-				}
-			}
+			updateDirection(InputHandler.Player1Up, InputHandler.Player1Left, InputHandler.Player1Down, InputHandler.Player1Right);
 			if(InputHandler.keyInputContains(InputHandler.Player1Attack)){
 				HitBox h = attack();
 				if(h != null){
@@ -62,28 +39,7 @@ public class HumanPlayer extends PlayableCharacter{
 				}
 			}
 		} else if(getTag().equals("Human.2")){
-			if(InputHandler.keyInputContains(InputHandler.Player2Up)){
-				if(!InputHandler.keyInputContains(InputHandler.Player2Down)){
-					move(0, -1);
-					setCurrYDir(-1);
-				}
-			} else if(InputHandler.keyInputContains(InputHandler.Player2Down)){
-				move(0, 1);
-				setCurrYDir(1);
-			} else {
-				setCurrYDir(0);
-			}
-			if(InputHandler.keyInputContains(InputHandler.Player2Left)){
-				if(!InputHandler.keyInputContains(InputHandler.Player2Right)){
-					move(-1, 0);
-					setCurrXDir(-1);
-				}
-			} else if(InputHandler.keyInputContains(InputHandler.Player2Right)){
-				move(1,  0);
-				setCurrXDir(1);
-			} else{
-				setCurrXDir(0);
-			}
+			updateDirection(InputHandler.Player2Up, InputHandler.Player2Left, InputHandler.Player2Down, InputHandler.Player2Right);
 			if(InputHandler.keyInputContains(InputHandler.Player2Attack)){
 				HitBox h = attack();
 				if(h != null){
@@ -96,4 +52,16 @@ public class HumanPlayer extends PlayableCharacter{
 		}
 		System.out.println(getTag() + " X: " + getCenterXPos() + " Y: " + getCenterYPos());
 	}
+	
+	public void updateDirection(KeyCode up, KeyCode left, KeyCode down, KeyCode right){
+		byte xMovement = 0;
+		byte yMovement = 0;
+		if(InputHandler.keyInputContains(right)){ xMovement++; }
+		if(InputHandler.keyInputContains(left)){ xMovement--; }
+		if(InputHandler.keyInputContains(up)){ yMovement--; }
+		if(InputHandler.keyInputContains(down)){ yMovement++; }
+		this.setCurrDir(Direction.getDir(xMovement, yMovement));
+		move(this.getCurrDir().getX(), this.getCurrDir().getY());
+	}
+	
 }
