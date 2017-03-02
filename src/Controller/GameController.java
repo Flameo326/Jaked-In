24ec236cc0ddc724f6c836fded941c6b2ec01023
Flameo326 <Flameo326@10.10.18.178>
2017-03-02 14:25:30 -0700

    Wall Generation Added
    Added Image Scaling
    
    Error Window

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -6,8 +6,14 @@ import java.util.Collections;
 import Models.Collision;
 import Models.Entity;
 import javafx.animation.AnimationTimer;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class GameController extends AnimationTimer {
 
@@ -15,11 +21,22 @@ public class GameController extends AnimationTimer {
 	private Canvas myCanvas;
 	private GraphicsContext g;
 	private Entity focusedEntity;
+	private Stage error;
+	private Label playPos;
 	
 	public GameController(Canvas myCanvas) {
 		this.myCanvas = myCanvas;
 		g = myCanvas.getGraphicsContext2D();
 		entities = new ArrayList<>();
+		
+		playPos = new Label();
+		
+		StackPane root = new StackPane(playPos);
+		Scene scene = new Scene(root, 300, 50);
+		
+		error = new Stage();
+		error.setScene(scene);
+		error.show();
 	}
 	
 	// This entire thing will be our "Run" method. It gets called constantly and updates accordingly.
@@ -45,6 +62,7 @@ public class GameController extends AnimationTimer {
 			// Print out Entity Information
 //			System.out.println(e.getClass().getSimpleName() + " X: " + e.getXPos() + " Y: " + e.getYPos());
 		}
+		playPos.setText("Player Center X: " + focusedEntity.getXPos() + " Y: " + focusedEntity.getYPos());
 		// Handles the graphical Rendering 
 		updateImage();
 	}
@@ -58,7 +76,7 @@ public class GameController extends AnimationTimer {
 		}
 		for(Entity e : entities){
 			g.drawImage(e.getImage(), e.getDisplayableXPos() - offsetX + (myCanvas.getWidth()/2),
-					e.getDisplayableYPos() - offsetY + (myCanvas.getHeight()/2));
+					e.getDisplayableYPos() - offsetY + (myCanvas.getHeight()/2), e.getWidth(), e.getHeight());
 		}	
 	}
 
