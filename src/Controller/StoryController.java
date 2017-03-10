ad826f1453b74319143ff46075501eae4cafb5fe
Flameo326 <Flameo326@10.0.0.173>
2017-03-09 20:31:25 -0700

    Added Hud

--- src/Controller/StoryController.java
+++ src/Controller/StoryController.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import Cutscene.Cutscene;
+import Cutscene.DialogCutscene;
 import Cutscene.Introduction;
 import Interfaces.Subscribable;
 import Models.Entity;
@@ -25,6 +26,8 @@ import Models.Map.Map;
 import Models.Map.MapGeneratorThread;
 import Models.Players.HumanPlayer;
 import Models.Players.PlayableCharacter;
+import Models.Upgrades.Upgrade;
+import Models.Weapon.ProjectileWeapon;
 import SpriteSheet.SpriteSheet;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -35,7 +38,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
@@ -58,6 +60,7 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 	private HumanPlayer player1;
 	private Floor1Map[] levels;
 	private long seed;
+	private long upgradeTime, enemyTime;
 	private int currentLevel;
 	private int lives;
 	
@@ -103,7 +106,6 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 	}
 	
 	public void createGameController(){
-		player1.setSpeed(5);
 		gc = new GameController(myCanvas, true);
 		gc.attach(this);
 		gc.addEntity(player1.getDisplayableEntities());
@@ -162,24 +164,45 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 		myCanvas.setOnKeyReleased((e) -> InputHandler.keyRelease(e));
 	}
 
+	//2359911335623836
+	
 	@Override
 	public void update(PlayableCharacter value) {
-		myCanvas.getGraphicsContext2D().setFont(new Font(20));
-		myCanvas.getGraphicsContext2D().setFill(Color.WHITE);
-		myCanvas.getGraphicsContext2D().fillText("Level " +(currentLevel+1), myCanvas.getWidth()*7/8, myCanvas.getHeight()/16);
+	displayHud();
 		
 		if(InputHandler.keyInputContains(KeyCode.ESCAPE)) { displayEscapeMenu(); }
-		// we should have a menu like pressing escape or something
-		// woud it be here?...
+
+		if(GameController.getTimer() >= upgradeTime){
+			// avg of 20 seconds
+			upgradeTime = GameController.getTimer()+ 10000000000l;
+			Upgrade u = levels[currentLevel].generateRandomUpgrade();
+			if(u != null) { 
+				levels[currentLevel].getMapObjects().add(u);
+				gc.addEntity(u); 
+			}
+		}
+		if(GameController.getTimer() >= enemyTime){
+			// Get current time + 30 seconds to 3 minutes ahead
+			enemyTime = GameController.getTimer()+ 20000000000l/(currentLevel+1);
+			PlayableCharacter enemy = levels[currentLevel].generateRandomEnemy();
+			if(enemy != null) { 
+				levels[currentLevel].addEntity(enemy.getDisplayableEntities());
+				gc.addEntity(enemy.getDisplayableEntities()); 
+			}
+		}
+		
 		if(!value.isAlive()){
 			System.out.println("Live lost"); 
 			if(--lives <= 0){
 				// displayLoss();
+				displayGameOver();
 				gc.stop();
 			} else {
 				// beginning of level?
-				player1.setXPos(levels[currentLevel].getEntrance().getXPos());
-				player1.setYPos(levels[currentLevel].getEntrance().getYPos());
+				startCutscene(new DialogCutscene(this, .5, "You have Died\n\n"+lives+" Memory Units Remaining"));
+				player1.setXPos(levels[currentLevel].getExit().getXPos());
+				player1.setYPos(levels[currentLevel].getExit().getYPos());
+				player1.setCurrentHealth(player1.getMaxHealth());
 			}
 		}
 	}
@@ -198,6 +221,71 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 		// KeyEvent to record input while playing
 		myCanvas.setOnKeyPressed((e) -> InputHandler.keyPress(e));
 		myCanvas.setOnKeyReleased((e) -> InputHandler.keyRelease(e));
+	}
+	
+	public void displayHud(){
+		GraphicsContext g = myCanvas.getGraphicsContext2D();
+		
+		// health
+		// Weapon -Damage + Time - Bullets
+		// Speed
+		// Reduction
+		// ForceField
+		
+		g.setFill(Color.rgb(0, 0, 0, .5));
+		g.fillRect(0, 0, myCanvas.getWidth(), myCanvas.getHeight()/5);
+		
+		g.setFont(new Font(20));
+		g.setFill(Color.WHITE);
+		g.fillText("Level " +(currentLevel+1), myCanvas.getWidth()*7/8, myCanvas.getHeight()/16);
+		
+		// Health
+		g.fillText("HP: " + player1.getCurrentHealth() + "/" + player1.getMaxHealth(),
+				myCanvas.getWidth()/4, 35);
+
+		// Construct the Weapontext
+		String weaponText = player1.getWeapon().getClass().getSimpleName()
+				.replace("Weapon", "").replace("Normal", "").replace("\\wProjectile", "");
+		String bullets = "";
+		if(player1.getWeapon() instanceof ProjectileWeapon){
+			bullets = ((ProjectileWeapon)player1.getWeapon()).getBullets()+"";
+		} else {
+			bullets = "\u221e";
+		}
+		
+		// Display Weapon
+		g.fillText(weaponText + ": " + bullets, myCanvas.getWidth()/4, 50);
+		
+		String damage = (player1.getWeapon().getDamage() + player1.getBaseDamage())
+				+"+(" + player1.getBonusDamage() + ") " + 
+				(player1.getBonusDamageLength() != 0 ? getFormattedTime(player1.getBonusDamageLength()) : "");
+		
+		// Display Damage
+		g.fillText("Damage: " + damage, myCanvas.getWidth()/4, 65);
+		
+		// Display Speed
+		g.fillText("Speed: " + player1.getSpeed() + "+(" + player1.getBonusSpeed() + ") " +
+				(player1.getBonusSpeedLength() != 0 ? getFormattedTime(player1.getBonusSpeedLength()) : ""),
+				myCanvas.getWidth()/4, 80);
+		
+		int y = 95;
+		
+		// Display Shield
+		if(player1.getDamageReduction() != 0 || player1.getBonusReduction() != 0){
+			g.fillText("Shield: " + player1.getDamageReduction() + 
+					"+(" + player1.getBonusReduction() + ") " + (player1.getBonusReductionLength()!=0 ?
+							getFormattedTime(player1.getBonusReductionLength()) : ""),
+							myCanvas.getWidth()/4, y);
+			y += 15;
+		}
+		
+		// Display ForceField
+		if(player1.getForceFieldLength() != 0){
+			g.fillText("ForceField: " + getFormattedTime(player1.getForceFieldLength()),
+					myCanvas.getWidth()/4, y);
+			y += 15;
+		}
+		
 	}
 	
 	public void displayEscapeMenu(){
@@ -283,6 +371,59 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 		});
 	}
 	
+	public void displayGameOver(){
+		int width = (int) myCanvas.getWidth();
+		int height = (int) myCanvas.getHeight();
+		GraphicsContext g = myCanvas.getGraphicsContext2D();
+		
+		g.setFill(Color.WHITE);
+		g.fillRect(width/4, height/8, width/2, height*3/4);
+		
+		g.setStroke(Color.BLACK);
+		g.strokeRect(width/4, height/8, width/2, height*3/4);
+		
+		g.setFont(new Font(48));
+		g.setFill(Color.BLACK);
+		g.setTextAlign(TextAlignment.CENTER);
+		g.fillText("Game Over!", width/2, height*2/8);
+		
+		int btnWidth = width*2/5;
+		int btnHeight =  height/16; 
+		int btnX = width/2 - btnWidth/2;
+		
+		g.setFont(new Font(32));
+		int backY = height*13/16 - btnHeight;
+		g.strokeRoundRect(btnX, backY+btnHeight/2, btnWidth, btnHeight, 5, 5);
+		g.fillText("Back to Main Menu", btnX + btnWidth/2, backY+btnHeight/2 + btnHeight*3/4, btnWidth);
+		
+		myCanvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent e) {
+				int x = (int)e.getSceneX();
+				int y = (int)e.getSceneY();
+				if(x > btnX && x < btnX + btnWidth){
+					// back button called
+					if(y > backY && y < backY + btnHeight){
+						HumanPlayer.resetHumanID();
+						Stage s = (Stage)myCanvas.getScene().getWindow();
+						if(s == null) { return; }
+						FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/StartFXML.fxml"));
+						BorderPane root = null;
+						try {
+							root = loader.load();
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						
+						Scene scene = new Scene(root, s.getScene().getWidth(), s.getScene().getHeight());
+						s.setScene(scene);
+						s.centerOnScreen();
+					}
+				}
+			}
+		});
+	}
+	
 	public void changeLevel(int i){
 		stop();
 		gc.removeEntity(levels[currentLevel].getMapObjects().toArray(new Entity[0]));
@@ -343,5 +484,19 @@ public class StoryController implements Initializable, Subscribable<PlayableChar
 		
 		
 		return controller;
+	}
+	
+	public String getFormattedTime(long l){
+//		String s = "";
+//		
+		long timeBetween = l - GameController.getTimer();
+		
+		long minute;
+		timeBetween -= (minute = timeBetween/60000000000l);
+		
+		long seconds;
+		timeBetween -= (seconds = timeBetween/1000000000l);
+		
+		return minute + ":" + seconds;
 	}
 }
